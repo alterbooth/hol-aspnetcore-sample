5895743ebcb0948823bab9261bda4163733cfbfe
tsubakimoto <tkmbmbr@gmail.com>
2020-09-13 22:19:26 +0900

    Add test project

---
+++ RazorPagesMovie.Test/Pages/AboutModelTest.cs
@@ -0,0 +1,16 @@
+﻿using RazorPagesMovie.Pages;
+using Xunit;
+
+namespace RazorPagesMovie.Test.Pages
+{
+    public class AboutModelTest
+    {
+        [Fact]
+        void OnGet()
+        {
+            var model = new AboutModel();
+            model.OnGet();
+            Assert.Equal("Your application description page.", model.Message);
+        }
+    }
+}
---
+++ RazorPagesMovie.Test/Pages/IndexModelTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPagesMovie.Pages;
+using Xunit;
+
+namespace RazorPagesMovie.Test.Pages
+{
+    public class IndexModelTest
+    {
+        [Fact]
+        void OnGet()
+        {
+            var model = new IndexModel();
+            var actual = model.OnGet() as RedirectToPageResult;
+            Assert.NotNull(actual);
+            Assert.Equal("/Movies/Index", actual.PageName);
+        }
+    }
+}
